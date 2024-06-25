fb9b7344cc2a9c93d2e6bbb8c0c84647b5afb88c
Saima003 <saima19@somaiya.edu>
2024-06-25 23:13:39 +0530

    Firebase URLs changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import GenerateIDOnRefresh from './components/GenerateIDOnRefresh';
+import HeroSection from './components/HeroSection';
 function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<GenerateIDOnRefresh />} />
+      {/* <Route path="/" element={<GenerateIDOnRefresh />} /> */}
+      <Route path="/" element={<HeroSection/>} />
     </Routes>
   </Router>
   );
--- src/components/GenerateIDOnRefresh.js
+++ src/components/GenerateIDOnRefresh.js
@@ -3,27 +3,32 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import { database } from '../FIrebaseConfig';
-
-
+import { db_database } from '../FIrebaseConfig';
 const GenerateIDOnRefresh = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
     const hash = location.hash.substring(1); // Remove the # from the hash
+    console.log('Current hash:', hash);
 
     if (!hash) {
       // If no ID in the URL, generate a new one
       const newId = uuidv4();
+      console.log('Generated new ID:', newId);
       navigate(`#${newId}`, { replace: true });
     } else {
       // Check if the ID already exists in Firebase
-      database.ref(`ids/${hash}`).once('value').then(snapshot => {
+      db_database.ref(`ids/${hash}`).once('value').then(snapshot => {
         if (!snapshot.exists()) {
           // If the ID does not exist, save it to Firebase
-          database.ref(`ids/${hash}`).set(true);
+          console.log('ID does not exist in Firebase. Saving new ID:', hash);
+          db_database.ref(`ids/${hash}`).set(true);
+        } else {
+          console.log('ID already exists in Firebase:', hash);
         }
+      }).catch(error => {
+        console.error('Error checking ID in Firebase:', error);
       });
     }
   }, [navigate, location]);
